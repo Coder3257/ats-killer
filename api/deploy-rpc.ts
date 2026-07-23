6e2b352b4f47a21b18079daca6a63474ca33e588
ravi-dev <ravimahto712@gmail.com>
2026-07-23 08:57:52 +0530

    chore: remove scratch files and hardcoded secrets from tracking

--- api/deploy-rpc.ts
+++ api/deploy-rpc.ts
@@ -24,7 +24,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     process.env.SUPABASE_DB_PASSWORD || "",
     process.env.SUPABASE_SERVICE_ROLE_KEY || "",
     "***REMOVED***",
-    "***REMOVED***",
     "postgres",
     "***REMOVED***"
   ].filter(Boolean);
